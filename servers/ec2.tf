08a07deacf4c6c2fbffb622c6f337b3143b2453e
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-26 22:32:07 +0530

    efgd

--- servers/ec2.tf
+++ servers/ec2.tf
@@ -27,6 +27,15 @@ resource "aws_instance" "catalogue" {
 
 }
 
+resource "aws_route53_record" "catalogue" {
+  zone_id = "aZ0607165JC9NKEPWSMH2"
+  name    = "catalogue.sstech.store"
+  type    = "A"
+  ttl     = 30
+  records = ["aws_instance.catalogue.private_ip"]
+  
+}
+
 resource "aws_instance" "mongodb" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -35,6 +44,15 @@ resource "aws_instance" "mongodb" {
         Name = "mongodb-tf"
     }
 
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = "aZ0607165JC9NKEPWSMH2"
+  name    = "mongodb.sstech.store"
+  type    = "A"
+  ttl     = 30
+  records = ["aws_instance.mongodb.private_ip"]
+  
 }
 
 resource "aws_instance" "user" {
@@ -47,6 +65,16 @@ resource "aws_instance" "user" {
 
 }
 
+resource "aws_route53_record" "user" {
+  zone_id = "aZ0607165JC9NKEPWSMH2"
+  name    = "user.sstech.store"
+  type    = "A"
+  ttl     = 30
+  records = ["aws_instance.user.private_ip"]
+  
+}
+
+
 resource "aws_instance" "cart" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -57,6 +85,16 @@ resource "aws_instance" "cart" {
 
 }
 
+resource "aws_route53_record" "cart" {
+  zone_id = "aZ0607165JC9NKEPWSMH2"
+  name    = "cart.sstech.store"
+  type    = "A"
+  ttl     = 30
+  records = ["aws_instance.cart.private_ip"]
+  
+}
+
+
 resource "aws_instance" "shipping" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -65,6 +103,15 @@ resource "aws_instance" "shipping" {
         Name = "shipping-tf"
     }
 
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = "aZ0607165JC9NKEPWSMH2"
+  name    = "shipping.sstech.store"
+  type    = "A"
+  ttl     = 30
+  records = ["aws_instance.shipping.private_ip"]
+  
 }
 
 resource "aws_instance" "payment" {
@@ -77,6 +124,16 @@ resource "aws_instance" "payment" {
 
 }
 
+resource "aws_route53_record" "payment" {
+  zone_id = "aZ0607165JC9NKEPWSMH2"
+  name    = "payment.sstech.store"
+  type    = "A"
+  ttl     = 30
+  records = ["aws_instance.payment.private_ip"]
+  
+}
+
+
 resource "aws_instance" "dispatch" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -87,6 +144,16 @@ resource "aws_instance" "dispatch" {
 
 }
 
+resource "aws_route53_record" "dispatch" {
+  zone_id = "aZ0607165JC9NKEPWSMH2"
+  name    = "dispatch.sstech.store"
+  type    = "A"
+  ttl     = 30
+  records = ["aws_instance.dispatch.private_ip"]
+  
+}
+
+
 resource "aws_instance" "redis" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -95,6 +162,15 @@ resource "aws_instance" "redis" {
         Name = "redis-tf"
     }
 
+}
+
+resource "aws_route53_record" "redis" {
+  zone_id = "aZ0607165JC9NKEPWSMH2"
+  name    = "redis.sstech.store"
+  type    = "A"
+  ttl     = 30
+  records = ["aws_instance.redis.private_ip"]
+  
 }
 
 resource "aws_instance" "mysql" {
@@ -107,6 +183,15 @@ resource "aws_instance" "mysql" {
 
 }
 
+resource "aws_route53_record" "mysql" {
+  zone_id = "aZ0607165JC9NKEPWSMH2"
+  name    = "mysql.sstech.store"
+  type    = "A"
+  ttl     = 30
+  records = ["aws_instance.mysql.private_ip"]
+  
+}
+
 resource "aws_instance" "rabbitmq" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
@@ -115,6 +200,19 @@ resource "aws_instance" "rabbitmq" {
         Name = "rabbitmq-tf"
     }
 
+}
+
+resource "aws_route53_record" "rabbitmq" {
+  zone_id = "aZ0607165JC9NKEPWSMH2"
+  name    = "rabbitmq.sstech.store"
+  type    = "A"
+  ttl     = 30
+  records = ["aws_instance.rabbitmq.private_ip"]
+
+}
+
+output "rabbitmypulicip" {
+  value = aws_instance.rabbitmq.public_ip
 }
 
 
