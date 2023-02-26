c47381af4f27020d7ac6b7038271efcd4737b514
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-26 22:26:30 +0530

    adfgb

--- servers/ec2.tf
+++ servers/ec2.tf
@@ -13,7 +13,7 @@ resource "aws_route53_record" "frontend" {
   name    = "frontend.sstech.store"
   type    = "A"
   ttl     = 30
-  records = [aws_instance.frontend.private_ip]
+  records = ["aws_instance.frontend.private_ip"]
   
 }
 
