31a3f0108f7de556cc7ed40ec4b5bbbc337919bd
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-27 11:45:52 +0530

    sdfg

---
+++ state/main.tf
@@ -0,0 +1,17 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-ss-b71"
+    key    = "state/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "myec2" {
+    ami = "ami-0a017d8ceb274537d"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [ "sg-094864f55a54f2a12" ]
+    tags = {
+        Name = "test1"
+    }
+
+}
