c10f240530b4705991f9437e8c7086c65b26411b
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-27 22:28:25 +0530

    asdf

---
+++ cond/main.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "myec2" {
+    ami = "ami-0a017d8ceb274537d"
+    instance_type = ""
+    vpc_security_group_ids = [ "sg-094864f55a54f2a12" ]
+    tags = {
+        Name = "conditional"
+    }
+
+}
