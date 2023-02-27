990fefe0e6f025666011df6b8c9fe11cb95c65a8
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-27 12:20:45 +0530

    dfgh

--- loops/main.tf
+++ loops/main.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "myec2" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
-  count                  = length(var.tags)
+  count                  = length(var.mytags)
   vpc_security_group_ids = ["sg-094864f55a54f2a12"]
   tags = {
-    Name = var.tags[ count.index ]
+    Name = var.mytags[ count.index ]
   }
 
 }
@@ -13,7 +13,7 @@ variable "servers" {
   default = ["frontend", "catalogue", "cart", "user"]
 }
 
-variable "tags" {
+variable "mytags" {
   default = {
     catalogue = {
       Name        = "catalogue"
