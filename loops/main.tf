05289aee4ba50b216ca2c0dc0857e6af382329ec
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-27 12:06:06 +0530

    sadfg

--- loops/main.tf
+++ loops/main.tf
@@ -1,10 +1,14 @@
 resource "aws_instance" "myec2" {
     ami = "ami-0a017d8ceb274537d"
     instance_type = "t3.micro"
-    count = 3
+    count = length(var.servers)
     vpc_security_group_ids = [ "sg-094864f55a54f2a12" ]
     tags = {
         Name = "test1"
     }
 
+}
+
+variable "servers" {
+  default = [ "frontend", "catalogue" ,"cart" ,"user" ]
 }
