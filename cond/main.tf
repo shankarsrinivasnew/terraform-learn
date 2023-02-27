7d6e39e2b7ece7389e791f0d516163aae13a2f91
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-27 23:11:20 +0530

    dgh

--- cond/main.tf
+++ cond/main.tf
@@ -8,4 +8,16 @@ resource "aws_instance" "myec2" {
 
 }
 
+resource "aws_instance" "myec2" {
+    ami = "ami-0a017d8ceb274537d"
+    instance_type = "t2.micro"
+    count = var.count == ""? 1 : var.count
+    vpc_security_group_ids = [ "sg-094864f55a54f2a12" ]
+    tags = {
+        Name = "count"
+    }
+
+}
+
 variable "instance_type" {}
+variable "count" {}
