040a52c7fe134c94f17ff7d70ebd4553a12cbcc3
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-02-28 23:15:37 +0530

    sadf

---
+++ provisioner/main.tf
@@ -0,0 +1,29 @@
+data "aws_ami" "myami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+resource "aws_instance" "myec2" {
+    ami =  data.aws_ami.myami.image_id
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [ "sg-094864f55a54f2a12" ]
+    tags = {
+        Name = "provisioner"
+    }
+
+    provisioner "myprovisioner" {
+        connection {
+          host = self.public_ip
+          user = "root"
+          password = "DevOps321"
+        }
+
+        inline = [
+            " echo hello",
+            "date"
+        ]
+      
+    }
+
+}
