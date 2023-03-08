14c5686dfc17e2dfaacb7d2c4d440dfd244427bb
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-08 14:51:38 +0530

    dsgf

--- prometheus-test/main.tf
+++ prometheus-test/main.tf
@@ -1,23 +1,34 @@
 
-resource "aws_spot_instance_request" "test1"  {
-    ami =  "ami-09badaa7430c2894e"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [ "sg-094864f55a54f2a12" ]
-    /* wait_for_fulfillment = true */
-    tags = {
-        Name = "prom-1"
-    }
+resource "aws_spot_instance_request" "node1" {
+  ami                    = "ami-0a36d3747768bc34b"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  wait_for_fulfillment   = true
 
+  tags = {
+    Name = "prom-test-server"
+  }
 }
 
-resource "aws_spot_instance_request" "test2"  {
-    ami =  "ami-09badaa7430c2894e"
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [ "sg-094864f55a54f2a12" ]
-    /* wait_for_fulfillment = true */
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-server"
+}
 
-    tags = {
-        Name = "prom-2"
-    }
+resource "aws_spot_instance_request" "node2" {
+  ami                    = "ami-0a36d3747768bc34b"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  wait_for_fulfillment   = true
 
+  tags = {
+    Name = "prom-test-node"
+  }
+}
+
+resource "aws_ec2_tag" "node2" {
+  resource_id = aws_spot_instance_request.node2.spot_instance_id
+  key         = "Name"
+  value       = "prom-test-node"
 }
