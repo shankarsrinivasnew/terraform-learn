d79de540f83e01f454570c87e57dbb4f36b9b454
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-08 14:52:43 +0530

    sdfg

--- prometheus-test/main.tf
+++ prometheus-test/main.tf
@@ -1,6 +1,6 @@
 
 resource "aws_spot_instance_request" "node1" {
-  ami                    = "ami-0a36d3747768bc34b"
+  ami                    = "ami-09badaa7430c2894e"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
   wait_for_fulfillment   = true
@@ -17,7 +17,7 @@ resource "aws_ec2_tag" "node1" {
 }
 
 resource "aws_spot_instance_request" "node2" {
-  ami                    = "ami-0a36d3747768bc34b"
+  ami                    = "ami-09badaa7430c2894e"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
   wait_for_fulfillment   = true
