0bef8d17050eabd98fb702d8c926c6ee790d4513
saku3071 <sakunalashankar.srinivas@rackspace.com>
2023-03-08 14:38:43 +0530

    dsfg

--- prometheus-test/main.tf
+++ prometheus-test/main.tf
@@ -1,9 +1,9 @@
 
 resource "aws_spot_instance_request" "test1"  {
-    ami =  ami-09badaa7430c2894e
+    ami =  "ami-09badaa7430c2894e"
     instance_type = "t3.micro"
     vpc_security_group_ids = [ "sg-094864f55a54f2a12" ]
-    wait_for_fullfillment = true
+    wait_for_fullfillment = "true"
     tags = {
         Name = "prom-1"
     }
@@ -11,10 +11,10 @@ resource "aws_spot_instance_request" "test1"  {
 }
 
 resource "aws_spot_instance_request" "test2"  {
-    ami =  ami-09badaa7430c2894e
+    ami =  "ami-09badaa7430c2894e"
     instance_type = "t3.micro"
     vpc_security_group_ids = [ "sg-094864f55a54f2a12" ]
-    wait_for_fullfillment = true
+    wait_for_fullfillment = "true"
 
     tags = {
         Name = "prom-2"
